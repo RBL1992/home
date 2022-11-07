55a79d30783b9b67ab76fbb9bab1aa15dbc36b9c
Roy <loganroyjr4@gmail.com>
2022-11-06 22:28:20 -0600

    added User and Home-Items models

---
+++ server/models/Home-Items.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat.js');
+
+const homeItemSchema = new Schema({
+    itemName: {
+        type: String,
+        required: true, 
+    },
+    itemImg: {
+
+    },
+    itemDescription: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100,
+        trim: true,
+    },
+    capturedAt: {
+        type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+    }
+})
---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require('mogoose');
+const bcrypt = require('bcrypt');
+
+const UserProfileSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    phoneNumber: {
+        type: String,
+        required: false,
+        // match: ?
+    },
+    homeItems: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Home-Items'
+        }
+    ]
+});
+
+UserProfileSchema.pre('save', async function (next){
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrpyt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+UserProfileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrpyt.compare(password, this.password);
+};
+
+const User = model ('User', UserProfileSchema);
+
+module.exports = User;
+
