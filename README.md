38c880591fe128495975207f1acf895b9121d3e6
Roy <loganroyjr4@gmail.com>
2022-11-06 21:57:56 -0600

    revised name

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Home Assistant
+# .MOVE
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/home-assistant',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/move',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
