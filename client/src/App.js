144f45ae29aa3a5d0650059d85fd8d06eec6f2d8
Roy <loganroyjr4@gmail.com>
2022-11-07 19:24:46 -0600

    created routes for pages in app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import Assistant from './pages/Assistant';
+import Friends from './pages/Friends';
+import Profile from './pages/Profile';
+import Rewards from './pages/Rewards.js'
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -10,7 +17,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-flex-start min-100-vh">
-       <h1>MEOW</h1>
+        <Header />
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/home" component={() => <Home />} />
+        <Route path="/assistant" component={() => <Assistant />} />
+        <Route path="/friends" component={() => <Friends />} />
+        <Route path="/profile" component={() => <Profile />} />
+        <Route path="/rewards" component={() => <Rewards />} />
+        <Footer />
       </div>
     </ApolloProvider>
   );
