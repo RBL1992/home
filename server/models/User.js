7a5862caeb7e68670bdc7b3b4ab78b7310d85e36
Jared <jjohns3@tulane.edu>
2022-11-09 14:12:22 -0600

    completed login, addUser, and many more GraphQL queries and mutations

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-// const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const UserProfileSchema = new Schema({
   firstName: {
@@ -18,6 +18,10 @@ const UserProfileSchema = new Schema({
     unique: true,
     match: [/.+@.+\..+/, "Must match an email address!"],
   },
+  password: {
+    type: String,
+    required: true,
+  },
   phoneNumber: {
     type: String,
     required: false,
@@ -32,17 +36,17 @@ const UserProfileSchema = new Schema({
   },
 });
 
-// UserProfileSchema.pre('save', async function (next){
-//     if (this.isNew || this.isModified('password')) {
-//         const saltRounds = 10;
-//         this.password = await bcrpyt.hash(this.password, saltRounds);
-//     }
-//     next();
-// });
+UserProfileSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
 
-// UserProfileSchema.methods.isCorrectPassword = async function (password) {
-//     return bcrpyt.compare(password, this.password);
-// };
+UserProfileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", UserProfileSchema);
 
