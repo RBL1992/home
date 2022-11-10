594bf90bb00befe285b59fa6ab82244a1462cd94
Jackson Myhre <jack421myhre@gmail.com>
2022-11-10 11:07:37 -0600

    Added card component and updated homepage. Started profile page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,46 @@
 import React from "react";
+import Card from '../components/Card';
+
+const styles = {
+  featureBtn: {
+    padding: 12,
+    border: '1px solid black'
+  }
+};
 
 function Home() {
-    return (
-      <h1>.home jackson ... rob make a cool logo</h1>
-    )
+  return (
+    <div>
+      <button style={styles.featureBtn} id='feature-btn'>Add Feature</button>
+
+      <div className="flex flex-row justify-center align-center">
+        <Card
+          name='Filter'
+          image='...'
+          room='kitchen'
+          maintenance='11/2/2022'
+        />
+        <Card
+          name='Filter'
+          image='...'
+          room='kitchen'
+          maintenance='11/2/2022'
+        />
+        <Card
+          name='Filter'
+          image='...'
+          room='kitchen'
+          maintenance='11/2/2022'
+        />
+        <Card
+          name='Filter'
+          image='...'
+          room='kitchen'
+          maintenance='11/2/2022'
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
