f11fb3a84d580309c6beb5d123c6259eef26f14f
RobThompson <rob.thomp15@gmail.com>
2022-11-11 11:11:58 -0600

    footer edit

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,15 +3,39 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 absolute left-0 bottom-0 right-0">
-      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-base text-gray-50">&copy; 2022 HOME - All rights reserved.</p>
-        </div>
+    <footer className="bg-gray-900 absolute bottom-0 left-0 right-0">
+    <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="mt-8 md:order-1 md:mt-0">
+        
+        <p className="text-center text-base text-gray-50">&copy; 2022 HOME - All rights reserved.</p>
       </div>
-    </footer>
+    </div>
+  </footer>
   )
 }
 
 
 export default Footer;
+
+
+ 
+
+
+{/* <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+    <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline">Contact</a>
+        </li>
+    </ul>
+</footer>  */}
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -29,7 +29,7 @@ class AuthService {
 
   login(idToken) {
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/home');
+    window.location.assign('/');
   }
 
   logout() {
