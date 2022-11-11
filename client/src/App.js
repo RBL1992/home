3bb755f560758150bfcd5ab7d7f3dea7bd7518ef
Jackson Myhre <jack421myhre@gmail.com>
2022-11-11 13:33:06 -0600

    Added landingpage component, added route to landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { setContext } from "@apollo/client/link/context";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from "./pages/Home";
+import Landingpage from "./pages/Landingpage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Assistant from "./pages/Assistant";
@@ -47,22 +48,23 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex flex-col justify-start h-full">
-         <Header />
-            <div className="container">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/assistant" element={<Assistant />} />
-            <Route path="/rewards" element={<Rewards />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/landing" element={<Landingpage />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/assistant" element={<Assistant />} />
+              <Route path="/rewards" element={<Rewards />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
           </div>
-          
-         <Footer />
-         </div>
-         
-        
+
+          <Footer />
+        </div>
+
+
       </Router>
     </ApolloProvider>
   );
