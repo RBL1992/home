b2f6674648c5c5fd895031aad5bcb8ab18b2581c
RobThompson <rob.thomp15@gmail.com>
2022-11-13 19:54:26 -0600

    updated logo. styling profile

--- client/src/components/RewardCards.js
+++ client/src/components/RewardCards.js
@@ -1,32 +1,11 @@
 import React from 'react';
 
 
-// const styles = {
-//   phoneButton: {
-//     padding: 16,
-//   },
-
-//   phoneButtonEl: {
-//     color: "#fff",
-//     width: 12,
-//     height: 12,
-//   },
-
-//   userCard: {
-//     border: "2px solid black",
-//     padding: 20,
-//   },
-
-//   userDetail: {
-//     margin: "24px 0",
-//   },
-// };
-
 const RewardCards = (props) => {
   console.log(props)
     return (
         <div>
-          <div>
+          <div className="mt-6 text-sm font-medium text-gray-900 px-6">
             <p>Usable Reward Points: 
               {props.profileInfo.currentHomePoints}
             </p>
