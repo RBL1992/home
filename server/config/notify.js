137775907c935b39e0c0d0518823df768cbfad78
Jared <jjohns3@tulane.edu>
2022-12-12 00:56:17 -0600

    comments about notification-catcher

--- server/config/notify.js
+++ server/config/notify.js
@@ -1,8 +1,9 @@
 // Must use require('<module-name>').default below because the index.js file in this package uses exports.default instead of module.exports
+// The following line also works because of the index.d.ts file located in server/types/notifme-sdk. It's behaving like a TypeScript module so we need to explicitly declare the module in a d.ts file in a folder the same name of the module. Not sure why this is behaving like TypeScript but this makes it work :)
 const NotifmeSdk = require('notifme-sdk').default;
 
 const notifmeSdk = new NotifmeSdk({
-  useNotificationCatcher: true // <= this sends all your notifications to the catcher running on port 1025
+  useNotificationCatcher: true // <= this sends all your notifications to the catcher running on port 1025. 
 })
 notifmeSdk
   .send({sms: {from: '+15000000000', to: '+15000000001', text: 'Hello, how are you?'}})
--- server/types/notifme-sdk/index.d.ts
+++ server/types/notifme-sdk/index.d.ts
@@ -1,3 +1,4 @@
 // The following line is needed to successfully import the notifme-sdk module into the server/config/notify.js file
-// 
+// This package is is behaving like TypeScript so we must explicitly declare the module. Not sure why it's behaving like TypeScript but this makes it work :)
+
 declare module 'notifme-sdk';
