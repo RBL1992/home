f4d6b15dd751723fd083255b79c08303633c82d4
Jared <jjohns3@tulane.edu>
2022-12-15 18:54:46 -0600

    working on including email trigger in feature creation

---
+++ server/config/sendgrid.js
@@ -0,0 +1,19 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'jaredsjohnson92@gmail.com', // Change to your recipient
+  from: 'jjohns3@tulane.edu', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
